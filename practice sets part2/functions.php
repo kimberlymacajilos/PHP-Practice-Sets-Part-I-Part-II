d4d20954866bab41b6ed28e8b183adf731b4a537
kimberlymacajilos <202280322@psu.palawan.edu.ph>
2025-02-18 21:12:12 +0800

    added tokenization, frequency calculation, and sorting functions

--- practice sets part2/functions.php
+++ practice sets part2/functions.php
@@ -12,7 +12,7 @@
     }
 
     /**
-     * Reads the user input from the form
+     * Reads the user input from the form.
      * 
      * @return empty string If the user did not input a text it will return an empty string.
      */
@@ -20,4 +20,46 @@
     function readUserInput(): string{
         return ($_SERVER["REQUEST_METHOD"] == "POST" ? $_POST['text'] : '');
     }
+
+    /**
+     * Tokenizes the text into an array words.
+     * 
+     * @param $text The input text from user that is going to be tokenized.
+     * @return array|false If the user did not input text, it will not be tokenized otherwise it will be tokenized.
+     */
+    function tokenization(string $text): array|false{
+        $pattern = '/\W+/';
+        return preg_split($pattern, strtolower($text), -1 PREG_SPLIT_NO_EMPTY);
+    }
+
+    /**
+     * Calculates the frequency of each word in the array words and ignores the common stop words.
+     * 
+     * @param $words Array words that is going to be calculated.
+     * @param $stopWords Array stop words that is going to be ignored in calculation of frequency.
+     * @return $countedWords It is an associative array where it has keys and values. The keys are the words that are not included in stop words, and the value is its number value.
+     */
+    function frequencyCalculation(array $words, array $stopWords): array{
+        $countedWords = array();
+
+        foreach ($words as $word){
+            if(!in_array($word, $stopWords)) {
+                $countedWords[$word] = isset($countedWords[$word]) ? $countedWords[$word] + 1:1;
+            }
+        }
+        return $countedWords;
+    }
+
+    /**
+     * Sorts the list of associative array counted words by frequency in ascending or descending order.
+     * 
+     * @param $countedWords The words that is going to be sorted.
+     * @param $sortOrder The order of the chosen sort of user.
+     * @return $countedWords Associative array that has keys(words) and values(frequency).
+     */
+    function sorting(array $countedWords, string $sortOrder){
+        $sortOrder == 'ascending' ? asort($countedWords) : arsort($countedWords);
+        return $countedWords;
+    }
+
 ?>
