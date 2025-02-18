03b5f963bea51619cef35e3f625e8c4d57cf01af
kimberlymacajilos <202280322@psu.palawan.edu.ph>
2025-02-18 19:59:00 +0800

    added functions php file for index

---
+++ practice sets part2/functions.php
@@ -0,0 +1,13 @@
+<?php
+    /**
+     * Reads the list of words in the stop words english text file and returns it as an array converted in lowercase.
+     * 
+     * FILE_IGNORE_NEW_LINES is used to used to skip the newline at end of each array elements.
+     * FILE_SKIP_EMPTY_LINES is use to skip empty lines in the file.
+     * @return array_map An array of stop words english converted to lowercase.
+     */
+    function readStopWords(): array{
+        $stopWords = file('stop_words_english.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        return array_map('strtolower' $stopWords);
+    }
+?>
